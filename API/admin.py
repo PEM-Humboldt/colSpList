5fbfd0505873969332d6ded3bf85ba5e6fe26d1f
marbotte <bottinmarius@gmail.com>
2022-04-17 16:17:39 -0500

    Added documentation of secutity and database admin function

--- API/admin.py
+++ API/admin.py
@@ -1,5 +1,21 @@
+"""
+Functions for the administration of the database, with a performance focus
+"""
+
 from taxo import synosAndParents
+
 def delTaxo_no_status(connection):
+    """
+    Suppresses the taxa which have no status (exotic, endemic or threatened). To use only when the database reaches its limits, because the presence of taxa even without statuses makes the API more efficient
+
+    Parameters:
+    -----------
+    connection: psycopg2 Connection
+        connection to the database
+    Returns:
+    --------
+    List of suppressed taxon identifiers
+    """
     deleted=[]
     cur = connection.cursor()
     SQL="SELECT cd_tax FROM exot UNION SELECT cd_tax FROM endemic UNION SELECT cd_tax FROM threat"
@@ -21,6 +37,17 @@ def delTaxo_no_status(connection):
     return deleted
 
 def delReference_no_status(connection):
+    """
+    Suppresses the references which are not associated with any status
+
+    Parameters:
+    -----------
+    connection: psycopg2 Connection
+        connection to the database
+    Returns:
+    --------
+    List of suppressed reference identifiers
+    """
     cur=connection.cursor()
     SQL= "WITH a AS(SELECT source AS cd_ref FROM taxon UNION SELECT cd_ref FROM ref_endem UNION SELECT cd_ref FROM ref_exot UNION SELECT cd_ref FROM ref_threat), b AS (SELECT r.cd_ref, a.cd_ref IS NOT NULL refed FROM refer r LEFT JOIN a USING(cd_ref)) DELETE FROM refer AS r USING b WHERE r.cd_ref=b.cd_ref AND NOT b.refed RETURNING r.cd_ref"
     cur.execute(SQL)
@@ -31,6 +58,17 @@ def delReference_no_status(connection):
     return deleted
 
 def delStatus_no_reference(connection):
+    """
+    Suppresses the statuses (endemism, alien/invasive or threatened) of species which are not associated with any bibliographic reference. If the API is used consistently, that should never happen...
+
+    Parameters:
+    -----------
+    connection: psycopg2 Connection
+        connection to the database
+    Returns:
+    --------
+    List of suppressed status identifiers (note: the status identifiers correspond to the taxon identifiers cd_tax
+    """
     cur=connection.cursor()
     SQL= "WITH a AS(SELECT cd_tax/*, BOOL_AND(cd_ref IS NULL) no_ref */FROM exot e LEFT JOIN ref_exot re USING (cd_tax) GROUP BY cd_tax HAVING BOOL_AND(cd_ref IS NULL)) DELETE FROM exot WHERE cd_tax IN (SELECT cd_tax FROM a) RETURNING cd_tax"
     cur.execute(SQL)
@@ -49,6 +87,17 @@ def delStatus_no_reference(connection):
     return deletedExot+deletedEndem+deletedThreat
 
 def delSyno_no_tax(connection):
+    """
+    Suppresses the synonyms which are not associated with any accepted taxa
+
+    Parameters:
+    -----------
+    connection: psycopg2 Connection
+        connection to the database
+    Returns:
+    --------
+    List of suppressed taxon identifiers
+    """
     cur=connection.cursor()
     SQL= "WITH a AS (SELECT t.cd_tax FROM taxon t LEFT JOIN taxon ta ON t.cd_syno=ta.cd_tax WHERE t.status='SYNONYM' AND ta.cd_tax IS NULL) DELETE FROM taxon WHERE cd_tax IN (SELECT cd_tax FROM a) RETURNING cd_tax"
     cur.execute(SQL)
@@ -58,5 +107,3 @@ def delSyno_no_tax(connection):
     connection.commit()
     return deleted
 
-    
-    
--- API/security.py
+++ API/security.py
@@ -1,3 +1,6 @@
+"""
+Security and user managements
+"""
 from flask import Flask, render_template, jsonify, current_app, g
 from flask_restful import Resource, Api
 import psycopg2
@@ -8,16 +11,64 @@ from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSign
 from errors_def import MissingArgError, AlreadyExistsDbError, DeleteMissingElementDbError, GrantExistingRightError, RevokeUnexistingRightError, UserNotFoundError 
 
 def hash_password(password):
+    """
+    Return the 'hash' (encrypted version) of a password
+    Parameter:
+    ==========
+    password: Str
+        password of the user
+    Returns:
+    ========
+    password_hash: Str 
+        encrypted version of the password
+    """
     password_hash = pwd_context.hash(password)
     return(password_hash)
 
 def user_exists(cursor,username):
+    """
+    Test whether a user exists, from its username
+    Parameters:
+    ==========
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    username: Str
+        Name of the user
+    """
     SQL = "SELECT count(*) AS nb FROM users WHERE username=%s"
     cursor.execute(SQL,[username])
     res = bool(cursor.fetchone()['nb'])
     return(res)
  
 def get_user(cursor, get_hash=False, **user):
+    """
+    Get the user information about a user from its id or username.
+
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    get_hash: Bool
+        Whether to return the encrypted the encrypted version of the user password
+    user: Dict
+        dictionary with the following elements:
+        id: Int
+            identifier of the user (uid)
+        username: Str
+            name of the user
+    Returns:
+    -------
+    user : Dict
+        dictionary with the following elements:
+        id: Int
+            identifier of the user (uid)
+        username: Str
+            name of the user
+        roles: List(Str)
+            roles of the user (associated with permissions in the API)
+        password_hash: Str
+            encrypted version of the user password
+    """
     if user.get('id') is not None:
         SQL = "SELECT * FROM users WHERE id=%s"
         cursor.execute(SQL,[user.get('id')])
@@ -44,16 +95,75 @@ def get_user(cursor, get_hash=False, **user):
     return user
 
 def valid_password(cursor,username,password):
+    """
+    Validate the password of the user and send a boolean (True if the password corresponds)
+
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    username : Str
+        name of the user
+    password : Str 
+        password of the user (to be tested against the encrypted password) 
+    Returns:
+    --------
+        Bool
+        if the password is the right one (when compared with the encrypted version kept in the database), send True
+    """
     user = get_user(cursor=cursor, get_hash=True, username=username)
     return pwd_context.verify(password, user.get('password_hash'))
 
 def insert_user(cursor,username,hash_pw):
+    """
+    Insert a new user in the database
+
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    username : Str
+        name of the user
+    hash_pw : Str 
+        encrypted password of the user (to be tested against the encrypted password) 
+
+    Returns:
+    --------
+    id : Int
+        identifier of the inserted user
+    """
     SQL = "INSERT INTO users(username, password_hash) VALUES(%s,%s) RETURNING id"
     cursor.execute(SQL,[username,hash_pw])
     res = cursor.fetchone()['id']
     return res
 
 def new_user(connection,**userArgs):
+    """
+    Create a new user in the database
+
+    Parameters:
+    -----------
+    connection: Psycopg2 connection
+        connection to the postgres database
+    userArgs: Dict
+        dictionary with the following parameters:
+        username: Str
+            name of the user
+        password: Str
+            password of the user
+    Returns:
+    --------
+    newId: Int
+        Identifier of the new user
+    username: Str
+        name of the user
+    Errors:
+    -------
+    MissingArgError: Exception
+        if the username or the password are missing, send this error with the corresponding missingArg argument
+    AlreadyExistsDbError: Exception
+        If the user already exists send this error
+    """
     cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     username = userArgs.get('username')
     password = userArgs.get('password')
@@ -70,6 +180,30 @@ def new_user(connection,**userArgs):
     return newId, username
 
 def delete_user(connection, **userArgs):
+    """
+    Delete a user from the database
+
+    Parameters:
+    ----------
+    connection: Psycopg2 connection
+        connection to the postgres database
+    userArgs: Dict
+        dictionary with the following elements
+        username: Str
+            name of the user
+        id: Str
+            identifier of the user
+    Returns:
+    --------
+    id: Int
+        Identifier of the user
+    username: Str
+        name of the user
+    Errors:
+    -------
+    DeleteMissingElementDbError: Exception
+        If the user do not exists, send this error and stop execution before applying change in the database
+    """
     cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     username = userArgs.get('username')
     if(not user_exists(cur,username)):
@@ -83,6 +217,28 @@ def delete_user(connection, **userArgs):
     return res, username
 
 def grant_user(cursor, **userArgs):
+    """
+    Grant the "user" role to a user
+    
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        id: Int
+            Identifier of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    GrantExistingRightError: Exception
+        If the user does not have the user permission and role, send this error and stops
+    """
     user=get_user(cursor, get_hash=False, **userArgs)
     if 'user' in user.get('roles'):
         raise GrantExistingRightError(user=user.get('username'), right='user')
@@ -93,6 +249,28 @@ def grant_user(cursor, **userArgs):
     return uidUpdated
 
 def revoke_user(cursor, **userArgs):
+    """
+    Revoke the "user" role to a user
+    
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        id: Int
+            Identifier of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    RevokeExistingRightError: Exception
+        If the user already has the user permission and role, send this error and stops
+    """
     user=get_user(cursor, get_hash=False, **userArgs)
     if not 'user' in user.get('roles'):
         raise RevokeUnexistingRightError(user=user.get('username'),right='user')
@@ -103,6 +281,28 @@ def revoke_user(cursor, **userArgs):
     return uidUpdated
 
 def grant_edit(cursor, **userArgs):
+    """
+    Grant the "edit" role to a user
+    
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        id: Int
+            Identifier of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    GrantExistingRightError: Exception
+        If the user does not have the edit permission and role, send this error and stops
+    """
     user=get_user(cursor, get_hash=False, **userArgs)
     if 'edit' in user.get('roles'):
         raise GrantExistingRightError(user=user.get('username'), right='edit')
@@ -113,6 +313,28 @@ def grant_edit(cursor, **userArgs):
     return uidUpdated
 
 def revoke_edit(cursor, **userArgs):
+    """
+    Revoke the "edit" role to a user
+    
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        id: Int
+            Identifier of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    RevokeExistingRightError: Exception
+        If the user does not have the edit permission and role, send this error and stops
+    """
     user=get_user(cursor, get_hash=False, **userArgs)
     if not 'edit' in user.get('roles'):
         raise RevokeUnexistingRightError(user=user.get('username'),right='edit')
@@ -123,6 +345,28 @@ def revoke_edit(cursor, **userArgs):
     return uidUpdated
 
 def grant_admin(cursor, **userArgs):
+    """
+    Grant the "admin" role to a user
+    
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        id: Int
+            Identifier of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    GrantExistingRightError: Exception
+        If the user does not have the admin permission and role, send this error and stops
+    """
     user=get_user(cursor, get_hash=False, **userArgs)
     if 'admin' in user.get('roles'):
         raise GrantExistingRightError(user=user.get('username'), right='admin')
@@ -133,6 +377,28 @@ def grant_admin(cursor, **userArgs):
     return uidUpdated
 
 def revoke_admin(cursor, **userArgs):
+    """
+    Revoke the "admin" role to a user
+    
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        id: Int
+            Identifier of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    RevokeExistingRightError: Exception
+        If the user does not have the admin permission and role, send this error and stops
+    """
     user=get_user(cursor, get_hash=False, **userArgs)
     if not 'admin' in user.get('roles'):
         raise RevokeUnexistingRightError(user=user.get('username'),right='admin')
@@ -143,6 +409,28 @@ def revoke_admin(cursor, **userArgs):
     return uidUpdated
 
 def change_password(cursor, **userArgs):
+    """
+    Change the password of the user (keep the new encrypted version in the database
+
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    userArgs: Dict
+        dictionary with the different element:
+        username: Str
+            name of the user
+        newPassword: Str
+            new password of the user
+    Returns:
+    --------
+    uidUpdated: Int
+        identifier of the updated user
+    Errors:
+    -------
+    MissingArgError: Exception
+        If the new password is not given, send this error
+    """
     user=get_user(cursor, **userArgs)
     if userArgs.get('newPassword') is None:
         raise MissingArgError(missingArg="'newPassword'")
@@ -152,14 +440,39 @@ def change_password(cursor, **userArgs):
     uidUpdated= cursor.fetchone()['id']
     return uidUpdated
 
-def generate_auth_token(connection,userId):
-    cur= connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+def generate_auth_token(userId):
+    """
+    Generate a time-limited (15000 seconds) token, with the user id encrypted version inside
+    
+    Parameters:
+    -----------
+    userId: Int
+        Identifier of the user in the database
+    (SECRET_KEY): Str
+        Environment variable containing the encryption key
+    Returns:
+    --------
+    token: Str
+        Token containing the user id and the validity time
+    """
     sc=os.environ.get('SECRET_KEY')
-    cur.close()
     s = Serializer(sc,expires_in = 15000)
     return s.dumps({'id':userId})
 
 def verify_auth_token(token,cur):
+    """
+    Verify the validity of a token, extract the user and send back the user information
+
+    Parameters:
+    -----------
+    (SECRET_KEY): Str
+        Environment variable containing the encryption key
+    cur: Psycopg2 cursor
+        Cursor of type RealDict in the postgres database
+    Returns
+    -------
+    None if the token does not correspond to any user, or the signature is expired, or is invalid, send the user information otherwise
+    """
     sc=os.environ.get('SECRET_KEY')
     s = Serializer(sc)
     try:
@@ -172,6 +485,24 @@ def verify_auth_token(token,cur):
     return user
 
 def get_user_list(cursor):
+    """
+    Get the list of the users and their roles/permissions
+
+    Parameters:
+    -----------
+    cursor: Psycopg2 cursor
+        cursor in the connection to the postgres database
+    Returns:
+    --------
+    res: List
+        List of dictionaries with the following elements:
+        uid: Int
+            identifier of the user in the database
+        username: Str
+            name of the user
+        roles: List(Str)
+            roles/permissions of the user (admin, edit, user)
+    """
     SQL = "SELECT id as uid, username,apiuser AS user,edit_auth AS edit, admin FROM users"
     cursor.execute(SQL)
     res=cursor.fetchall()
